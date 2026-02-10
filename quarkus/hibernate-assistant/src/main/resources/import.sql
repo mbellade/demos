31a87d7795a3299b902184770c223fb84eb0c46f
Marco Belladelli <marcobladel@gmail.com>
2026-02-10 11:43:31 +0100

    Misc fixes and start final cleanup

--- quarkus/hibernate-assistant/src/main/resources/import.sql
+++ quarkus/hibernate-assistant/src/main/resources/import.sql
@@ -2,14 +2,13 @@ insert into company_table (id, name, city, street) values(1, 'Red Hat', 'Raleigh
 insert into company_table (id, name, city, street) values(2, 'IBM', 'Armonk', 'Orchard Road');
 insert into company_table (id, name, city, street) values(3, 'Belladelli Giovanni Jewelry', 'Pegognaga', 'Via Roma');
 insert into company_table (id, name, city, street) values(4, 'MB startup', null, null);
-
 insert into company_table (id, name, city, street) values(7, 'Apple', 'Cupertino', 'Apple Park');
 insert into company_table (id, name, city, street) values(5, 'Alphabet', 'Mountain View', 'Googleplex');
 insert into company_table (id, name, city, street) values(6, 'American Express', 'New York City', 'Vesey Street');
 
 insert into employee_table (id, first_name, last_name, salary, works_at) values(1, 'Marco', 'Belladelli', 500000, 1);
 insert into employee_table (id, first_name, last_name, salary, works_at) values(2, 'Andrea', 'Boriero', 100000, 1);
-insert into employee_table (id, first_name, last_name, salary, works_at) values(3, 'Mario', 'Fusco', 200000, 2);
+insert into employee_table (id, first_name, last_name, salary, works_at) values(3, 'Luca', 'Molteni', 200000, 2);
 insert into employee_table (id, first_name, last_name, salary, works_at) values(4, 'Flavia', 'Galeotti', 300000, 3);
 
 insert into project_table (name, start_date) values('Quarkus', date '2019-03-20');
